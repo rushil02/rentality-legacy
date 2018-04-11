12b3e25c08abc3a6062162b7860757bdef035d5f
rushil <rushil0195@gmail.com>
2018-04-11 17:03:07 +1000

    add house !need FIX for edit! templates

--- house/views.py
+++ house/views.py
@@ -16,6 +16,7 @@ def info(request, house_uuid):
         return render(request, 'house/info.html', {'house': house})
 
 
+# FIXME: how to edit houses and maintain continuation, use form wizards?
 def add_house_details(request, uuid, next_url, FormClass, template):
     if uuid:
         try:
@@ -48,29 +49,29 @@ def add_house_form1(request, uuid=None):
 
 def add_house_form2(request, uuid=None):
     next_url = 'house:'
-    template = 'house/add_house/specs.html'
+    template = 'house/add_house/date_cost_details.html'
     return add_house_details(request, uuid, next_url, HouseDetailsForm2, template)
 
 
 def add_house_photos(request, uuid=None):
     next_url = 'house:'
-    template = 'house/add_house/specs.html'
+    template = 'house/add_house/photos.html'
     return add_house_details(request, uuid, next_url, HousePhotosForm, template)
 
 
 def add_house_form4(request, uuid=None):
     next_url = 'house:'
-    template = 'house/add_house/specs.html'
+    template = 'house/add_house/description.html'
     return add_house_details(request, uuid, next_url, HouseDetailsForm3, template)
 
 
 @login_required
-def add_edit_house(request, form_num, uuid):
+def add_edit_house(request, form_num, uuid=None):
     func_di = {
-        '1': add_house_form1,
-        '2': add_house_form2,
-        '3': add_house_photos,
-        '4': add_house_form4,
+        1: add_house_form1,
+        2: add_house_form2,
+        3: add_house_photos,
+        4: add_house_form4,
     }
 
     try:
