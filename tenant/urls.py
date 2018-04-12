dac0a2faf5fd2d0f772b3caec3aac61cd86732f0
rushil <rushil0195@gmail.com>
2018-04-12 22:16:30 +1000

    search bugs

--- tenant/urls.py
+++ tenant/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('info/<uuid:house_pref_uuid>', info, name='info'),
     path('pref', HousePreferenceCreateView.as_view(), name='house_pref'),
     path('sl', shortlisted_houses, name='shortlist'),
-    path('search', search_tenant_page, name='search_house'),
-    path('search-api', search_tenant_api, name='search_house_api')
+    path('search', search_tenant_page, name='search'),
+    path('search-api', search_tenant_api, name='search_api')
 ]
