1e09f08f2872ce56914fa221a5ee963edc1268f5
rushil <rushil0195@gmail.com>
2018-04-30 14:41:49 +1000

    refresh server script

--- bin/refresh_server.sh
+++ bin/refresh_server.sh
@@ -4,6 +4,8 @@ now=$(date +%Y%m%d%H%M%Z)
 f_name="sql_dump_$now.sql"
 pg_dump rentality > ${f_name}
 dropdb rentality
+sudo -u postgres createdb rentality
+sudo -u postgres psql -U postgres -d postgres -c "grant all privileges on database rentality to root;"
 python manag.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn_rentality
