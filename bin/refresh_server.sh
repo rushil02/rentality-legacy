f4f29d1285494b222cf6bc1ef69493895f9f360c
rushil <rushil0195@gmail.com>
2018-04-30 14:36:53 +1000

    refresh server commands

--- bin/refresh_server.sh
+++ bin/refresh_server.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+now=$(date +%Y%m%d%H%M%Z)
+f_name="sql_dump_$now.sql"
+pg_dump rentality > ${f_name}
+dropdb rentality
+python manag.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn_rentality
 sudo systemctl restart nginx
+python manage.py createsuperuser
