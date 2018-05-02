e0e94b21ea527c8f3d3085286a2850ca26b85662
rushil <rushil0195@gmail.com>
2018-05-02 15:20:21 +1000

    bash files

--- bin/update_server_branch.sh
+++ bin/update_server_branch.sh
@@ -3,11 +3,7 @@
 now=$(date +%Y%m%d%H%M%Z)
 f_name="../sql_dump/sql_dump_$now.sql"
 pg_dump rentality > ${f_name}
-dropdb rentality
-sudo -u postgres createdb rentality
-sudo -u postgres psql -U postgres -d postgres -c "grant all privileges on database rentality to root;"
 python manage.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn_rentality
 sudo systemctl restart nginx
-python manage.py createsuperuser
