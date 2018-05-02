8180e7a7ab8a84d1ebe164d4e0357078570dc0b6
rushil <rushil0195@gmail.com>
2018-05-03 07:30:30 +1000

    bin file

--- bin/update_server_branch.sh
+++ bin/update_server_branch.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+echo "Dumping Data"
 now=$(date +%Y%m%d%H%M%Z)
 f_name="../sql_dump/sql_dump_$now.sql"
 pg_dump rentality > ${f_name}
+echo "Done"
+
 git pull
+
+echo "Activating Python Virtual Environment"
+source ../py3_env_rentality/bin/activate
+echo "Done"
+
+pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic
+
+echo "Reloading daemon"
 sudo systemctl daemon-reload
+echo "Done"
+echo "Restarting Gunicorn"
 sudo systemctl restart gunicorn_rentality
+echo "Done"
+echo "Restarting Nginx"
 sudo systemctl restart nginx
+echo "Done"
