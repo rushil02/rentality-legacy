805b9fa743b671ee4fcb521db8e4e3c88816e412
rushil <rushil0195@gmail.com>
2018-05-03 18:57:05 +1000

    admin panel update

--- tenant/admin.py
+++ tenant/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from tenant.models import TenantProfile, HousePreference
 
+
+class HousePreferenceAdmin(admin.ModelAdmin):
+    list_display = ('tenant', 'status')
+
+
 admin.site.register(TenantProfile)
-admin.site.register(HousePreference)
+admin.site.register(HousePreference, HousePreferenceAdmin)
