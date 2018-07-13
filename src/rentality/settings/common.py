0b15ff4bf5eba31c8dca1103289d562bff0a852e
rushil <rushil0195@gmail.com>
2018-07-14 01:45:45 +1000

    ssl fixes

--- src/rentality/settings/common.py
+++ src/rentality/settings/common.py
@@ -3,6 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+
 def get_env_var(var_name):
     try:
         var = os.environ[var_name]
