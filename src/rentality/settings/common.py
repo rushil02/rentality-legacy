0c7fa5fce70fa4d86cc1256efac7af7b558da86c
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2018-07-21 18:04:03 +0530

    Changed Env File

--- src/rentality/settings/common.py
+++ src/rentality/settings/common.py
@@ -1,11 +1,20 @@
 import os
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def get_env_var(var_name):
     try:
-        var = os.environ[var_name]
-    except Exception as e:
-        raise e
-    return var
+        filename = '/run/secrets/' + var_name
+        if os.path.isfile(filename):
+            with open(filename) as f:
+                return f.read()
+        else:
+            raise FileNotFoundError
+    except:
+        try:
+            secret_data = dotenv_values('/run/secrets/flask_secret')
+            return secret_data[var_name]
+        except:
+            return os.environ[var_name]
