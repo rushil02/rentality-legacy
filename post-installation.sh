673b019fa0f7d1ad92e35a87dad88dfb221c8089
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2018-07-25 03:37:34 +0530

    Added sync servies in web services

--- post-installation.sh
+++ post-installation.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-docker-compose exec web python manage.py makemigrations
-docker-compose exec web python manage.py migrate
-docker-compose exec web python manage.py initialize_site
-docker-compose exec web python manage.py cities --import=postal_code
+docker-compose exec web sh sync_services.sh
+
+read -p "Import Postal Codes? [y/n]" opt
+
+if [ "$opt" = "y" ]; then
+    docker-compose exec web python manage.py cities --import=postal_code
+fi
+
 read -p "Create Superuser? [y/n]" opt
 
 if [ "$opt" = "y" ]; then
