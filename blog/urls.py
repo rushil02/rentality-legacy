426b206a51bf181e7134c8b257d6960568e914ff
rushil <rushil0195@gmail.com>
2018-10-12 14:30:57 +1100

    Blog page 2 - Things you should know before moving to Australia

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('what-is-vacancy-tax-how-to-avoid', TemplateView.as_view(template_name='blogs/vacancy_tax_intro.html'),
-         name='vacancy_tax_intro')
+         name='vacancy_tax_intro'),
+    path('know-before-moving-to-aus', TemplateView.as_view(template_name='blogs/before_moving_to_aus.html'),
+         name='before_moving_to_aus')
+
 ]
