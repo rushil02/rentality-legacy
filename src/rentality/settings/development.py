1af189936870459d4a06cdc3a339af59e7efd576
rushil <rushil0195@gmail.com>
2018-11-11 21:21:03 +1100

    allowed_hosts changed to all for development

--- src/rentality/settings/development.py
+++ src/rentality/settings/development.py
@@ -20,4 +20,4 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_dev", "media_root")
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
