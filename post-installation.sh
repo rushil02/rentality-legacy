f5d2f2fd5f4bb3385f01b16b73f3b2aacc23422c
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2018-11-20 01:47:32 +0530

    Fixture for cities added

--- post-installation.sh
+++ post-installation.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 docker-compose exec web sh sync_services.sh
 
-read -p "Import Postal Codes? [y/n]" opt
+read -p "Import Cities Data? [y/n]" opt
 
 if [ "$opt" = "y" ]; then
-    docker-compose exec web python manage.py cities --import=postal_code
+    docker-compose exec web python manage.py loaddata cities_dump.json
 fi
 
-read -p "Import Countries? [y/n]" opt
+# read -p "Import Postal Codes? [y/n]" opt
 
-if [ "$opt" = "y" ]; then
-    docker-compose exec web python manage.py cities --import=country
-fi
+# if [ "$opt" = "y" ]; then
+#     docker-compose exec web python manage.py cities --import=postal_code
+# fi
+
+# read -p "Import Countries? [y/n]" opt
+
+# if [ "$opt" = "y" ]; then
+#     docker-compose exec web python manage.py cities --import=country
+# fi
 
 read -p "Create Superuser? [y/n]" opt
 
