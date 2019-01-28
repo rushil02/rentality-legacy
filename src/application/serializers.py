672982317735e38d46c2a0c8139eee6406ecd5df
rushil <rushil0195@gmail.com>
2019-01-29 03:36:39 +1100

    application serializers

--- src/application/serializers.py
+++ src/application/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
+
 from application.models import Application
 
 
-class ApplicationSerializer(serializers.ModelSerializer):
+class ApplicationPublicSerializer(serializers.ModelSerializer):
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+
     class Meta:
         model = Application
         fields = '__all__'
-        read_only_fields = ('house_meta', 'tenant_meta', 'rent', 'fee', 'meta', 'status', 'tenant', 'date')
+        read_only_fields = (
+            'house', 'house_meta', 'tenant_meta', 'rent', 'fee', 'meta', 'status', 'tenant', 'date', 'ref_code'
+        )
+
+
+class BookingInfoSerializer(serializers.Serializer):
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+    guests = serializers.IntegerField()
+    promo_codes = serializers.ListField(child=serializers.CharField())
+
+
+class BookingAmountDetailsSerializer(serializers.Serializer):
+    total_amount = serializers.DecimalField(max_digits=15, decimal_places=2)
+    service_fee = serializers.DecimalField(max_digits=12, decimal_places=2)
+    payable_amount = serializers.DecimalField(max_digits=15, decimal_places=2)
+    weekly_rent = serializers.DecimalField(max_digits=10, decimal_places=2)
+    discount = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+
+class ApplicationSerializer(serializers.Serializer):
+    class Meta:
+        model = Application
