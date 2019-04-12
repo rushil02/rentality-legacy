77a211f30f30f8d6fc5cdf3ae7d7b9d4eaed0c61
ViditTiwari <vidittiwari95@gmail.com>
2019-04-12 16:59:08 +0530

    Staff Panel House List and Display

--- src/staff/urls.py
+++ src/staff/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from staff.views import create_edit_promo_code, applications_list, application_details, application_resend_email
+from staff.views.houses import houses_list, house_details
 from .views import home
 
 app_name = 'staff'
@@ -15,6 +16,10 @@ urlpatterns = [
     # Applications
     path('applications/list', applications_list, name='application_list'),
     path('applications/details/<uuid:uuid>', application_details, name='application_details'),
-    path('application/resend-email/<str:entity>/<uuid:uuid>', application_resend_email, name='application_resend_email')
+    path('application/resend-email/<str:entity>/<uuid:uuid>', application_resend_email, name='application_resend_email'),
+
+    # Houses
+    path('houses/list', houses_list, name='house_list'),
+    path('houses/details/<uuid:uuid>', house_details, name='house_details')
 
 ]
