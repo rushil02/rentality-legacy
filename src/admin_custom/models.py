62426a399f76e66190ab4a06cfff12507635197a
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2019-05-01 03:10:22 +0530

    Added fields in activity log

--- src/admin_custom/models.py
+++ src/admin_custom/models.py
@@ -79,15 +79,15 @@ class ActivityLog(models.Model):
     )
     level = models.CharField(max_length=1, choices=CHOICES)
 
-    # OPERATION_TYPE = (
-    #     ('C', 'Create'),
-    #     ('R', 'Read'),
-    #     ('U', 'Update'),
-    #     ('D', 'Delete'),
-    # )
-    # operation_type = models.CharField(max_length=1, choices=OPERATION_TYPE)
+    OPERATION_TYPE = (
+        ('C', 'Create'),
+        ('R', 'Read'),
+        ('U', 'Update'),
+        ('D', 'Delete'),
+    )
+    operation_type = models.CharField(max_length=1, choices=OPERATION_TYPE)
 
-    # action = models.CharField(max_length=128)
+    action = models.CharField(max_length=128)
 
     meta_info = JSONField(null=True, blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
