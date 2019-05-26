db26a6d283c388a9ef6e79905c398d5e303693cb
Pranav Aggarwal <pranav.aggarwal06@gmail.com>
2019-05-26 21:43:41 +0530

    Added new search query

--- src/elastic_search/views.py
+++ src/elastic_search/views.py
@@ -4,6 +4,7 @@ from elasticsearch import NotFoundError
 from elasticsearch_dsl import Q
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from elastic_search.core.utils import create_connection, get_index_name
@@ -90,3 +91,78 @@ def search_house(request):
     s = s.sort('-create_time', 'rent')[slice_start:slice_end]
     results = s.execute().to_dict()
     return JsonResponse(results['hits']['hits'], safe=False, status=status.HTTP_200_OK)
+
+
+class HouseSearchView(APIView):
+    filter_fields = ['obj_pk', ]
+
+    def get(self, request, *args, **kwargs):
+        in_loc_id = request.GET.get('loc_sugg', None)
+        location = request.GET.get('location', '')
+
+        if (not location or location == 'null') and not in_loc_id:
+            return Response({'details': "Invalid Data"}, status=status.HTTP_400_BAD_REQUEST)
+
+        start_date = request.GET.get('start-date', '')
+        end_date = request.GET.get('end-date', '')
+        slice_start = int(request.GET.get('pagination-start', 0))
+        slice_end = int(request.GET.get('pagination-end', 10))
+
+        in_loc = None
+        if in_loc_id and in_loc_id != 'null':
+            try:
+                resp = Location.get(id=in_loc_id)
+            except NotFoundError:
+                pass
+            else:
+                if resp.geo_point:
+                    in_loc = resp.geo_point
+                else:
+                    in_loc = None
+        
+        s = House.search()
+
+        if start_date and end_date and "null" not in [start_date, end_date]:
+            start_date = datetime.strptime(start_date, "%d-%m-%Y").date()
+            end_date = datetime.strptime(end_date, "%d-%m-%Y").date()
+
+            s = s.filter(
+                    Q({"nested": {
+                            "path" : "availabilities",
+                            "query" : {
+                                "range" : {
+                                    "availabilities.date_range" : {
+                                        "gte": start_date.strftime("%d/%m/%Y"),
+                                        "lte": end_date.strftime("%d/%m/%Y"),
+                                        "format": "dd/MM/yyyy",
+                                        "relation": "contains"
+                                    }
+                                }
+                            }
+                        }
+                    })
+                )
+        
+        if in_loc and location:
+            s = s.filter(
+                    'geo_distance', distance='50km', geo_point=in_loc, distance_type='plane'
+                ).query(
+                    Q("multi_match", query=location, fields=['address', 'location'], fuzziness="AUTO")
+                )
+        elif in_loc:
+            s = s.filter(
+                    'geo_distance', distance='50km', geo_point=in_loc, distance_type='plane'
+                )
+        else:
+            s = s.query(
+                    Q("multi_match", query=location, fields=['address', 'location'], fuzziness="AUTO")
+                )
+        s = s.sort('-create_time', 'rent')[slice_start:slice_end]
+        results = s.execute().to_dict()
+        output = map(self.filter_data, results['hits']['hits'])
+        return Response(output, status=status.HTTP_200_OK)
+    
+    def filter_data(self, es_record):
+        for filter_field in self.filter_fields:
+            del es_record['_source'][filter_field]
+        return es_record
