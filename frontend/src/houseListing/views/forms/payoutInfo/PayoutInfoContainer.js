bad85211ca8b4fd958785950ecf324140dfa3c46
Vidit <vidittiwari95@gmail.com>
2020-03-31 17:26:59 +0530

    PayoutContainer WIP

--- frontend/src/houseListing/views/forms/payoutInfo/PayoutInfoContainer.js
+++ frontend/src/houseListing/views/forms/payoutInfo/PayoutInfoContainer.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import BillingInfoContainer from "./BillingInfoContainer";
-import {checkHousePayoutInfo} from "houseListing/services";
-import {ResponseLoadingSpinner} from "core/loadingSpinners/LoadingSpinner";
+import { checkHousePayoutInfo } from "houseListing/services";
+import { ResponseLoadingSpinner } from "core/loadingSpinners/LoadingSpinner";
 import PGInfoContainer from "./PGInfoContainer";
 
 /**
@@ -15,65 +15,58 @@ export default class PayoutInfoContainer extends Component {
         super(props);
         this.state = {
             verifying: true,
-            statusBI: 'Verifying'
+            statusBI: "Verifying"
         };
     }
 
     componentDidMount() {
-        this.props.navContext.data.loadForm(this.formID, this.onSave, 'initial', "Billing and Bank Information");
+        this.props.navContext.data.loadForm(this.formID, this.onSave, "initial", "Billing and Bank Information");
+        this.verifyPayoutInfo();
+    }
 
+    componentWillUnmount() {
+        this.props.navContext.data.unloadForm();
+    }
+
+    verifyPayoutInfo = () => {
         checkHousePayoutInfo(this.props.houseUUID)
             .then(result => {
-                this.setState(prevState => (
-                    {
+                this.setState(prevState => ({
+                    ...prevState,
+                    verifying: false,
+                    statusBI: "Complete"
+                }));
+            })
+            .catch(error => {
+                if (error.response.status === 406 && error.response.data.code === "BIM") {
+                    this.setState(prevState => ({
                         ...prevState,
                         verifying: false,
-                        statusBI: 'Complete'
-                    })
-                );
-
-            }).catch(error => {
-            if (error.response.status === 406 && error.response.data.code === "BIM") {
-                this.setState(prevState => (
-                    {
-                        ...prevState,
-                        verifying: false,
-                        statusBI: 'Incomplete'
-                    })
-                );
-            }
-        });
+                        statusBI: "Incomplete"
+                    }));
+                }
+            });
 
         // this.props.navContext.sync();
     };
 
-    componentWillUnmount() {
-        this.props.navContext.data.unloadForm();
-    };
-
     onSave = () => {
-        return new Promise(function (resolve, reject) {
-            resolve()
-
-        })
+        return new Promise(function(resolve, reject) {
+            resolve();
+        });
     };
 
     render() {
         return (
             <React.Fragment>
-                {this.state.verifying ?
-                    <ResponseLoadingSpinner height={"20vh"} message={"Verifying your Billing details"}/> : null
-                }
-                <div className="col-md-12" style={{marginTop: "50px"}}>
-                    <BillingInfoContainer
-                        status={this.state.statusBI}
-                    />
-                    <PGInfoContainer
-                        statusBI={this.state.statusBI}
-                    />
+                {this.state.verifying ? (
+                    <ResponseLoadingSpinner height={"20vh"} message={"Verifying your Billing details"} />
+                ) : null}
+                <div className="col-md-12" style={{ marginTop: "50px" }}>
+                    <BillingInfoContainer status={this.state.statusBI} verifyPayoutInfo={this.verifyPayoutInfo  } />
+                    <PGInfoContainer statusBI={this.state.statusBI} />
                 </div>
             </React.Fragment>
-        )
+        );
     }
-
 }
