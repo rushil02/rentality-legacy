6d84447eb71f8d05f59fefff925544d7df02b858
rushil <rushil0195@gmail.com>
2020-06-18 10:22:45 +1000

    Migration to Gatsby P3

--- frontend/app/src/pages/blog/index.js
+++ frontend/app/src/pages/blog/index.js
@@ -2,18 +2,12 @@ import React from "react"
 
 import Layout from "components/layout"
 import SEO from "components/seo"
-// import Navbar from "../components/navbar/views/Navbar";
-import Footer from "components/footer/Footer"
-// import Loadable from "@loadable/component"
 import BlogRouter from "components/blog/Router"
 
 const BlogHomePage = () => (
     <Layout>
         <SEO title="Blog" />
         <BlogRouter />
-        {/*<Navbar/>*/}
-        {/* <LoadableApp /> */}
-        {/*<Footer/>*/}
     </Layout>
 )
 
