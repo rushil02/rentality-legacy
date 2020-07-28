438a71208040cac2ebd83a577c9c4696c79914ce
rushil <rushil0195@gmail.com>
2020-07-29 07:56:31 +1000

    nginx config changes

--- nginx/bin/switch-to-online.sh
+++ nginx/bin/switch-to-online.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 cp -f /home/bin/online.conf /etc/nginx/conf.d/default.conf
+
+echo "Checking for 'web' service ..."
+sh /home/bin/wait-for-web.sh
+echo "Checking for 'frontend' files ..."
+sh /home/bin/wait-for-frontend-initial-build.sh
+
 sh /docker-entrypoint.sh nginx -s reload
