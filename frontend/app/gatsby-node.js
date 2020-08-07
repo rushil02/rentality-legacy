d0bdc625c2c52b25f2f857f414b3ac47d8e3cc7c
rushil <rushil0195@gmail.com>
2020-08-07 10:44:29 +1000

    Babelrc changes; gatsby node changes

--- frontend/app/gatsby-node.js
+++ frontend/app/gatsby-node.js
@@ -1,30 +1,8 @@
 const axios = require("axios")
 
-function getAllArticles() {
-    return new Promise(function (resolve, reject) {
-        axios
-            .get("http://web:8000/blog/all-articles")
-            .then(response => {
-                resolve(response.data)
-            })
-            .catch(error => {
-                reject(error)
-            })
-    })
-}
-
-function getAllTagsAndArticles() {
-    return new Promise(function (resolve, reject) {
-        axios
-            .get("http://web:8000/blog/all-tags-and-articles")
-            .then(response => {
-                resolve(response.data)
-            })
-            .catch(error => {
-                reject(error)
-            })
-    })
-}
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
 function slugify(verbose) {
     return verbose
@@ -38,24 +16,29 @@ function slugify(verbose) {
         .replace(/--+/g, "-")
 }
 
-exports.createPages = async ({ actions: { createPage } }) => {
-    const allArticles = await getAllArticles()
-    const allTagsAndArticles = await getAllTagsAndArticles()
+exports.createPages = async ({actions: {createPage}}) => {
+    try {
+        const allArticles = await axios.get(`http://web:8000/blog/all-articles/${process.env.INTERNAL_ACCESSOR}`)
+        const allTagsAndArticles = await axios.get(`http://web:8000/blog/all-tags-and-articles/${process.env.INTERNAL_ACCESSOR}`)
 
-    // Create a page for each Article.
-    allArticles.forEach(article => {
-        createPage({
-            path: `/blog/${article.slug}`,
-            component: require.resolve("./src/templates/blog/SingleBlogTemplate.js"),
-            context: { article },
+        // Create a page for each Article.
+        allArticles.data.forEach(article => {
+            createPage({
+                path: `/blog/${article.slug}`,
+                component: require.resolve("./src/templates/blog/SingleBlogTemplate.js"),
+                context: {article},
+            })
         })
-    })
 
-    allTagsAndArticles.forEach(tagAndArticles => {
-        createPage({
-            path: `/blog/tag/${slugify(tagAndArticles.verbose)}`,
-            component: require.resolve("./src/templates/blog/TagRelatedBlogsTemplate.js"),
-            context: { tagAndArticles },
+        allTagsAndArticles.data.forEach(tagAndArticles => {
+            createPage({
+                path: `/blog/tag/${slugify(tagAndArticles.verbose)}`,
+                component: require.resolve("./src/templates/blog/TagRelatedBlogsTemplate.js"),
+                context: {tagAndArticles},
+            })
         })
-    })
+    } catch (e) {
+        console.info(e)
+        throw e
+    }
 }
--- frontend/bin/entrypoint.prod-dev.sh
+++ frontend/bin/entrypoint.prod-dev.sh
@@ -3,7 +3,7 @@
 # Use this file to run gatsby's server with prod built files by
 # replacing `entrypoint` in docker-compose.yml
 
-cp -f /run/secrets/COMM_ENV /app/.env.development
+cp -f /run/secrets/COMM_ENV /app/.env.production
 unset GATSBY_EXPERIMENTAL_PAGE_BUILD_ON_DATA_CHANGES
 gatsby clean
 node /app/bin/wait_for_backend.js
